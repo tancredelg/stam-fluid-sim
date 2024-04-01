f30905d1c1eb456c82511555731118a3474f92c7
Tancrède Lamort de Gail <tancredelamortdegail@gmail.com>
2024-04-01 19:25:12 -0400

    fixed step 8

--- src/fluid.py
+++ src/fluid.py
@@ -272,9 +272,9 @@ class HelloWorld(mglw.WindowConfig):
 		a = dt * kappa * self.nx * self.ny
 		
 		for k in range(self.iterations):
-			for i in range(1, self.nx+1):
-				for j in range(1, self.ny+1):
-					q[j,i] = ((q0[j,i] + a * (q[j,i-1] + q[j,i+1] + q[j-1,i] + q[j+1,i])) / (1 + 4*a))
+			for i in range(1, self.ny+1):
+				for j in range(1, self.nx+1):
+					q[i,j] = (q0[i,j] + a * (q[i,j-1] + q[i,j+1] + q[i-1,j] + q[i+1,j])) / (1 + 4*a)
 			q = self.set_boundary(q, boundary_type)
 		
 		return q
@@ -307,7 +307,7 @@ class HelloWorld(mglw.WindowConfig):
 		# Loop over each particle in the grid
 		for p in range(self.particles.shape[0]):
 			i, j, a, b = self.xy_to_ij(*self.particles[p])
-
+			'''
 			# Get 4 nearest cells' velocities (4 cases)
 			if b < 0.5:  # Bottom half of cell
 				if a < 0.5:  # Bottom-left
@@ -335,11 +335,16 @@ class HelloWorld(mglw.WindowConfig):
 			# Calculate new interpolation "weights" across the 4 cells
 			A = a + 0.5 if a < 0.5 else a - 0.5
 			B = b + 0.5 if b < 0.5 else b - 0.5
+			'''
 			
 			# Calculate velocity of particle, and perform forward step 
-			v = v11 * (1-A) * (1-B) + v12 * (1-A) * B + v21 * A * (1-B) + v22 * A * B
-			v_new = self.particles[p] + dt * v
-			self.particles[p] = np.clip(v_new,[self.xl, self.yl],[self.xh, self.yh])
+			# v = v11*(1-A)*(1-B) + v12*(1-A)*B + v21*A*(1-B) + v22*A*B
+			v = (self.curr_v[i,j]*(1-a)*(1-b) +
+				 self.curr_v[i+1,j]*(1-a)*b +
+				 self.curr_v[i,j+1]*a*(1-b) +
+				 self.curr_v[i+1,j+1]*a*b)
+			new_pos = self.particles[p] + dt * v
+			self.particles[p] = np.clip(new_pos,[self.xl, self.yl],[self.xh, self.yh])
 		
 		return
 
@@ -358,24 +363,24 @@ class HelloWorld(mglw.WindowConfig):
 		dt0x, dt0y = dt * self.nx, dt * self.ny
 		
 		# q[row, col]	gv[row, col, x|y]
-		for i in range(1, self.nx+1):
-			for j in range(1, self.ny+1):
-				x = i - dt0x * gv[j,i,0]
-				y = j - dt0y * gv[j,i,1]
+		for i in range(1, self.ny+1):
+			for j in range(1, self.nx+1):
+				x = j - dt0x * gv[i,j,0]
+				y = i - dt0y * gv[i,j,1]
 				
 				x = np.clip(x, 0.5, self.nx + 0.5)
 				y = np.clip(y, 0.5, self.ny + 0.5)
-				
+
 				i0, j0 = int(y), int(x)
 				i1, j1 = i0 + 1, j0 + 1
-				
 				s1, t1 =  y - i0, x - j0
 				s0, t0 = 1 - s1, 1 - t1
 
 				# print(f"i={i}, x={x}, i0={i0}, i1={i1}, s1={s1}, s0={s0}")
 				# print(f"j={j}, y={y}, j0={j0}, j1={j1}, t1={t1}, t0={t0}\n")
 
-				q[j,i] = s0 * (t0 * q0[i0,j0] + t1 * q0[i0,j1]) + s1 * (t0 * q0[i1,j0] + t1 * q0[i1,j1])
+				q[i,j] = (s0 * (t0 * q0[i0,j0] + t1 * q0[i0,j1]) +
+						  s1 * (t0 * q0[i1,j0] + t1 * q0[i1,j1]))
 				
 		return self.set_boundary(q, boundary_type)
 
@@ -398,8 +403,8 @@ class HelloWorld(mglw.WindowConfig):
 		'''Applies the buoyancy force to the velocity field.'''
 		# TODO: STEP 8: Complete this function
 		mean_tp = np.mean(self.curr_tp)
-		next_v_y = self.curr_v[:,:,1] - self.beta * dt * (self.curr_v[:,:,1] - mean_tp) 
-		self.curr_v[:,:,1] = self.set_boundary(next_v_y, "vertical")
+		self.curr_v[:,:,1] += self.beta * dt * (self.curr_tp - mean_tp) 
+		self.curr_v[:,:,1] = self.set_boundary(self.curr_v[:,:,1], "vertical")
 		
 
 	def scalar_step(self, dt):
